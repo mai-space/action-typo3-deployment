ae9231ad5b53cbdafa61f2fab7865ea25f3e039c
Joel Mai <38419872+Inf166@users.noreply.github.com>
2023-12-30 16:29:17 +0100

    [TASK] Added verbose error messages

--- entrypoint.sh
+++ entrypoint.sh
@@ -236,16 +236,25 @@ set -e
 
 echo "⧗ Backing up previous release..."
 cd "$REMOTE_PATH/releases"
+ls -la
+
 if [ -L "previous" ]
 then
     previous_target=$(readlink "previous")
-    if [ -n "$previous_target" ] && [ -d "$previous_target" ]
+    if [ -z "$previous_target" ]
     then
-        rm "previous"
-    else
-        echo "Error: Invalid target for symbolic link 'previous'. Target is empty or does not exist."
+        echo "Error: Target for symbolic link 'previous' is empty."
         exit 1
+    elif [ ! -d "$previous_target" ]
+    then
+        echo "Error: Target directory for symbolic link 'previous' does not exist: $previous_target"
+        exit 1
+    else
+        rm "previous"
+        echo "Removed 'previous' symbolic link."
     fi
+else
+    echo "Symbolic link 'previous' does not exist."
 fi
 
 if [ -d "current" ]
@@ -253,14 +262,21 @@ then
     if [ -L "current" ]
     then
         current_target=$(readlink "current")
-        if [ -n "$current_target" ] && [ -d "$current_target" ]
+        if [ -z "$current_target" ]
         then
-            ln -s "$current_target" "previous"
-        else
-            echo "Error: Invalid target for symbolic link 'current'. Target is empty or does not exist."
+            echo "Error: Target for symbolic link 'current' is empty."
             exit 1
+        elif [ ! -d "$current_target" ]
+        then
+            echo "Error: Target directory for symbolic link 'current' does not exist: $current_target"
+            exit 1
+        else
+            ln -s "$current_target" "previous"
+            echo "Created 'previous' symbolic link."
         fi
     fi
+else
+    echo "Directory 'current' does not exist."
 fi
 EOF
 chmod +x update_symlinks.sh
